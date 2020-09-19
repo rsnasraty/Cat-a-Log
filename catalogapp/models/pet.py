c41b98232829a2d1b7ff7a1c3d555f8a843d220d
Roxanne Nasraty <rsnasraty@gmail.com>
2020-09-18 23:19:32 -0500

    Almost finished with MVT

--- catalogapp/models/pet.py
+++ catalogapp/models/pet.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 
 # blueprint to show what the thing will look like
 
@@ -16,6 +16,6 @@ def create_pet(sender, instance, created, **kwargs):
     if created:
         Pet.objects.create(user=instance)
         
-@receiver(post-save, sender=User)
+@receiver(post_save, sender=User)
 def save_pet(sender, instance, **kwargs):
     instance.pet.save()
